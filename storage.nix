be5f52f0a81fa959b0ab8d92b8a64b1c05eab7e2
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2023-09-29 03:59:32 +0000

    Add and comment out storage rbind

--- storage.nix
+++ storage.nix
@@ -1,38 +1,47 @@
 { config, lib, ... }:
 
 {
-  fileSystems."/" =
-    { device = "tmpfs";
-      fsType = "tmpfs";
-      options = [ "defaults" "size=64G" "mode=755" ];
-    };
+  fileSystems."/" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = [ "defaults" "size=64G" "mode=755" ];
+  };
 
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/85cbadbd-85a6-420b-8534-2597d9f8da21";
-      fsType = "btrfs";
-      options = [ "subvol=@nixos/nix" "compress=zstd" ];
-    };
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/85cbadbd-85a6-420b-8534-2597d9f8da21";
+    fsType = "btrfs";
+    options = [ "subvol=@nixos/nix" "compress=zstd" ];
+  };
 
-  fileSystems."/mnt/shared" =
-    { device = "/dev/disk/by-uuid/4b501aec-8608-486f-a51c-d5af4312c8b1";
-      fsType = "btrfs";
-      options = [ "nofail" ];
-    };
+  fileSystems."/mnt/shared" = {
+    device = "/dev/disk/by-uuid/4b501aec-8608-486f-a51c-d5af4312c8b1";
+    fsType = "btrfs";
+    options = [ "nofail" ];
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/56F6-1AA8";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/56F6-1AA8";
+    fsType = "vfat";
+  };
 
-  fileSystems."/var/cache/ccache" =
-    { device = "/nix/ccache";
-      fsType = "none";
-      options = [ "bind" ];
-    };
+  fileSystems."/var/cache/ccache" = {
+    device = "/nix/ccache";
+    fsType = "none";
+    options = [ "bind" ];
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/b56a944d-29ba-4866-87ed-18100e4c608f"; }
-    ];
+  # fileSystems."/mnt/root" = {
+  #   device = "/";
+  #   fsType = "none";
+  #   depends = [ "/" ];
+  #   options = [ "rbind" ];
+  # };
+
+  swapDevices = [
+    {
+      device = "/dev/disk/by-uuid/b56a944d-29ba-4866-87ed-18100e4c608f";
+    }
+  ];
 
   # Enable persistent storage
   environment.persistence."/nix/persist" = {
