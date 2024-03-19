9a70e23bda2bc20b1e699bb2de0ab75d63edaacc
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-03-19 16:12:01 +0000

    snapshot@03/19/2024-16:12:01

--- users/inme/default.nix
+++ users/inme/default.nix
@@ -58,12 +58,12 @@
       rcFiles."nix-helper.xsh".text = ''
         def init():
           def __rebuild_system_local(args):
-            ![nom build --builders "" "/etc/nixos#nixosConfigurations.\"{$(uname -n)}\".config.system.build.toplevel" &&
+            ![nom build --builders "" "/etc/nixos#nixosConfigurations.\"{$(uname -n)}\".config.system.build.toplevel" && \
             nixos-rebuild --use-remote-sudo --flake /etc/nixos @(args)]
 
           def __rebuild_system_remote(args):
             ![nom build -j0 "/etc/nixos#nixosConfigurations.\"{$(uname -n)}\".config.system.build.toplevel" --option substituters \
-              "https://nix-community.cachix.org https://cache.nixos.org/ http://nix-serve.router.local/" &&
+              "https://nix-community.cachix.org https://cache.nixos.org/ http://nix-serve.router.local/" && \
             nixos-rebuild -j0 --use-remote-sudo --flake /etc/nixos @(args)]
 
           def __commit_nixos_config(args):
