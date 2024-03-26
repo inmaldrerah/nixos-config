5b222d50d80c8546eba42b1d1525e0df709816f9
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-03-26 05:46:05 +0000

    snapshot@03/26/2024-05:46:05

--- users/inme/default.nix
+++ users/inme/default.nix
@@ -55,6 +55,7 @@
 
     programs.xonsh = {
       enable = true;
+      rcFiles."test.xsh".text = "";
       rcFiles."nix-helper.xsh".text = ''
         def init():
           def __rebuild_system_local(args):
