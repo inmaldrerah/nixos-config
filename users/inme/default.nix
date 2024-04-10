3adf60e4989680d2934df409cf11bb9c1fab2459
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-04-10 06:11:40 +0000

    Fix set oneshot boot entry to the target in loader.conf

--- users/inme/default.nix
+++ users/inme/default.nix
@@ -58,7 +58,7 @@
           def rebuild_system(args):
             if __rebuild_system(args):
               target = p"/boot/loader/loader.conf".read_text().split()[3]
-              print(f"Setting default to @saved and oneshot to {target}")
+              print(f"setting default to @saved and oneshot to {target}")
               ![sudo bootctl set-default "@saved"] && \
                 ![sudo bootctl set-oneshot @(target)]
           
