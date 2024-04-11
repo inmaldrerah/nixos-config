d06c23059b8ef703f3f4494331fff30a28792f7a
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-04-11 13:37:01 +0000

    snapshot@04/11/2024-13:37:01

--- hardware.nix
+++ hardware.nix
@@ -13,9 +13,25 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.supportedFilesystems = [ "overlay" "btrfs" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [  ];
-  boot.kernelPackages = pkgs.pkgsGnu.linuxPackages_6_7_hardened;
+  boot.kernelPackages = pkgs.pkgsGnu.linuxPackages_latest;
   boot.kernelParams = [
     "amd_pstate=active"
+    "security=selinux"
+  ];
+  boot.kernelPatches = [
+    {
+      name = "selinux-config";
+      patch = null;
+      extraConfig = ''
+        SECURITY_SELINUX y
+        SECURITY_SELINUX_BOOTPARAM n
+        SECURITY_SELINUX_DISABLE n
+        SECURITY_SELINUX_DEVELOP y
+        SECURITY_SELINUX_AVC_STATS y
+        SECURITY_SELINUX_CHECKREQPROT_VALUE 0
+        DEFAULT_SECURITY_SELINUX n
+      '';
+    }
   ];
 
   hardware.opengl.driSupport = true;
