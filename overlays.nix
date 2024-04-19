5e893adca35a3335e4afa097e77e411fd5f0f93d
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-04-19 15:03:54 +0000

    snapshot@04/19/2024-15:03:54

--- overlays.nix
+++ overlays.nix
@@ -69,7 +69,7 @@ let
         '';
       in
         (super.xorg.xkeyboardconfig_custom { inherit layouts; }).overrideAttrs (old: {
-          buildInputs = old.buildInputs ++ [ self.automake ];
+          buildInputs = old.buildInputs ++ [ super.automake ];
           postPatch   = with lib; concatStrings (mapAttrsToList patchIn options);
         });
     })
