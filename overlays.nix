fb72edca36dc192ea268f5d5ea2955974847f340
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-04-19 15:05:42 +0000

    snapshot@04/19/2024-15:05:42

--- overlays.nix
+++ overlays.nix
@@ -69,7 +69,7 @@ let
         '';
       in
         (super.xorg.xkeyboardconfig_custom { inherit layouts; }).overrideAttrs (old: {
-          postPatch   = with lib; concatStrings (mapAttrsToList patchIn options);
+          postPatch = with lib; concatStrings (mapAttrsToList patchIn options);
         });
     })
   ];
