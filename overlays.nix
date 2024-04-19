c4649398a387a7b3354dff87ab3cc82249ca5648
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-04-19 16:52:08 +0000

    snapshot@04/19/2024-16:52:08

--- overlays.nix
+++ overlays.nix
@@ -29,7 +29,7 @@ let
     })
     neovim-nightly-overlay.overlay
     (self: super: {
-      xorg = lib.recursiveUpdate super.xorg {
+      xorg = super.xorg // {
         xkeyboardconfig_custom = { layouts ? { }, options ? { } }:
         let
           patchIn = filename: option:
