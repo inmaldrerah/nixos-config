bac7eda0115c9b5435124163d62730cf1329158d
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-04-19 15:32:43 +0000

    snapshot@04/19/2024-15:32:43

--- xkb/default.nix
+++ xkb/default.nix
@@ -5,17 +5,17 @@
       us-qwpr = {
         description = "US QWPR layout";
         languages = [ "eng" ];
-        symbolsFile = xkb/symbols/us-qwpr;
+        symbolsFile = symbols/us-qwpr;
       };
     };
     options = {
       super = {
         description = "Super behavior";
         optionDescriptions.arrow_keys = "Super + Up/Down/Left/Right is mapped to PageUp/PageDown/Home/End";
-        symbolsFile = xkb/symbols/super;
+        symbolsFile = symbols/super;
       };
     };
-    xkbPatched = pkgs.xorg.xkeyboardconfig_custom { inherit layouts; };
+    xkbPatched = pkgs.xorg.xkeyboardconfig_custom { inherit layouts; inherit options; };
   in "${xkbPatched}/etc/X11/xkb";
   environment.sessionVariables.XKB_CONFIG_ROOT = config.services.xserver.xkb.dir;
   services.xserver.exportConfiguration = config.services.xserver.displayManager.startx.enable
