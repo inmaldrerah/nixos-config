403f81cd7696316ba83ed104dbcc27212088fbe8
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-04-27 04:19:20 +0000

    snapshot@04/27/2024-04:19:20

--- network.nix
+++ network.nix
@@ -6,8 +6,8 @@ rec {
 
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  networking.wireless.iwd.enable = true;
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # networking.wireless.iwd.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
