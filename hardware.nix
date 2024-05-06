fa9268cc814d1120609c8a6332359d5b8aa9753e
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-05-06 10:04:24 +0000

    snapshot@05/06/2024-10:04:24

--- hardware.nix
+++ hardware.nix
@@ -13,7 +13,7 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.supportedFilesystems = [ "overlay" "btrfs" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [  ];
-  boot.kernelPackages = pkgs.pkgsGnu.linuxPackages_testing;
+  boot.kernelPackages = pkgs.pkgsGnu.linuxPackages_latest;
   boot.kernelParams = [
     "amd_pstate=active"
   ];
