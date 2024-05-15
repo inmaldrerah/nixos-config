00f678f048be35c7f4db62b3a67eefddeae6f193
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-05-15 03:15:49 +0000

    snapshot@05/15/2024-03:15:49

--- users/inme/default.nix
+++ users/inme/default.nix
@@ -57,13 +57,11 @@
           
           def rebuild_system(args):
             rebuild_status = __rebuild_system(args)
-            if rebuild_status:
-              target = p"/boot/loader/loader.conf".read_text().split()[3]
-              print(f"setting default to @saved and oneshot to {target}")
-              return ![sudo bootctl set-default "@saved"] && \
-                ![sudo bootctl set-oneshot @(target)]
-            else:
-              return rebuild_status
+            target = p"/boot/loader/loader.conf".read_text().split()[3]
+            print(f"setting default to @saved and oneshot to {target}")
+            return rebuild_status && \
+              ![sudo bootctl set-default "@saved"] && \
+              ![sudo bootctl set-oneshot @(target)]
           
           def toggle_nix_local(args):
             if "HOME" in ''${...} and $HOME != "" and pf"{$HOME}/.nix-local".is_file():
