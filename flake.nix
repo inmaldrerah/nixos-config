4370214d8f46e1f4a46a945fcfb57b45fa15b410
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-08-09 03:45:55 +0000

    snapshot@08/09/2024-03:45:55

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable-small";
+      # url = "github:nixos/nixpkgs/nixos-unstable-small";
+      url = "github:nixos/nixpkgs/nixos-unstable";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
