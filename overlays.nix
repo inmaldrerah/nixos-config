5ffc0c520fc0ec986a3b9f8664f42fd22775fb0a
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-08-27 05:03:53 +0000

    snapshot@08/27/2024-05:03:53

--- overlays.nix
+++ overlays.nix
@@ -33,7 +33,7 @@ let
     })
     nur-linyinfeng.overlays.default
     (self: super: {
-      inherit (nixpkgs-extension.packages."${config.nixpkgs.hostPlatform}") switch-to-configuration-ng;
+      inherit (nixpkgs-extension.packages."x86_64-linux") switch-to-configuration-ng;
     })
   ];
   stdenv = (import nixpkgsInput {
