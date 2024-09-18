c088ac46d26fc841c86c378f795f7c6774d7d2ee
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-09-18 00:38:31 +0000

    snapshot@09/18/2024-00:38:31

--- network.nix
+++ network.nix
@@ -9,6 +9,9 @@ rec {
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   # networking.wireless.iwd.enable = true;
 
+  networking.networkmanager.dns = "none";
+  networking.nameservers = [ "::1" ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
@@ -20,6 +23,20 @@ rec {
   security.sudo.extraConfig = ''
     Defaults env_keep += "http_proxy https_proxy ftp_proxy rsync_proxy no_proxy"
   '';
+
+  services.dnsmasq.enable = true;
+  services.dnsmasq.settings = {
+    listen-address = [ "::1" ];
+    no-dhcp-interface = [ "*" ];
+    cache-size = 10000;
+    server = [
+      "/centaur-centauri.ts.net/100.100.100.100"
+      "/nju.edu.cn/210.28.129.251"
+      "::1#5553"
+      "/cn/223.5.5.5"
+      "1.1.1.1"
+    ];
+  };
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
