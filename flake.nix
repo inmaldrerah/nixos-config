38f2981f0e7638785ae637908e4580685db851e5
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2024-11-25 03:23:04 +0000

    snapshot@11/25/2024-03:23:04

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable-small";
+      url = "github:nixos/nixpkgs/nixos-unstable";
       # url = "github:nixos/nixpkgs/nixos-unstable";
       # url = "github:inmaldrerah/nixpkgs/stc-merge-restart-start";
     };
