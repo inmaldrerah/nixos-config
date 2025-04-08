de296cefeb4dd58cd60b9c1ca1276bc56651817a
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2025-04-08 09:01:53 +0000

    snapshot@04/08/2025-09:01:53

--- device/thinkbook-16-plus-nixos/default.nix
+++ device/thinkbook-16-plus-nixos/default.nix
@@ -6,14 +6,7 @@
 
   # Automatically log in to account
   # Password already verified at startup
-  services.greetd = {
-    enable = true;
-    settings = rec {
-      initial_session = {
-        command = "${lib.getExe config.programs.hyprland.package}";
-        user = "inme";
-      };
-      default_session = initial_session;
-    };
+  programs.hyprland = {
+    withUWSM = true;
   };
 }
