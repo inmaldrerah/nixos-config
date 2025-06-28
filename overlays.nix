d0f8b65e8922fea5c57cfb4f0fac1bc05168c2ce
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2025-06-28 16:14:47 +0000

    snapshot@06/28/2025-16:14:47

--- overlays.nix
+++ overlays.nix
@@ -31,7 +31,7 @@ let
     nur-linyinfeng.overlays.default
     (self: super: {
       hyprlandPlugins.hyprscrolling = super.hyprlandPlugins.hyprscrolling.overrideAttrs (old: {
-        src = lib.fetchFromGitHub {
+        src = super.fetchFromGitHub {
           owner = "hyprwm";
           repo = "hyprland-plugins";
           tag = "dd28351a6181c37553cca1ce437f0049dcd3ee5f";
