b1d0dfedae6530535f63513ecef80bcbc6f3d730
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2025-10-23 03:30:20 +0000

    snapshot@10/23/2025-03:30:20

--- overlays.nix
+++ overlays.nix
@@ -1,9 +1,9 @@
-{ config, lib, nixpkgsInput, nur-linyinfeng, ... }:
+{ config, nixpkgsInput, nur-linyinfeng, ... }:
 
 let
   packageOverlays = [
-    (pkgs: {
-      shell = pkgs.writeShellApplication {
+    (final: prev: {
+      shell = prev.writeShellApplication {
         name = "shell-starter";
         text = ''
           /bin/sh $HOME/.shell $@
@@ -11,31 +11,6 @@ let
       };
     })
     nur-linyinfeng.overlays.default
-    (final: prev: {
-      ccacheWrapper = prev.ccacheWrapper.override {
-        extraConfig = ''
-          export CCACHE_COMPRESS=1
-          export CCACHE_DIR="${config.programs.ccache.cacheDir}"
-          export CCACHE_UMASK=007
-          if [ ! -d "$CCACHE_DIR" ]; then
-            echo "====="
-            echo "Directory '$CCACHE_DIR' does not exist"
-            echo "Please create it with:"
-            echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
-            echo "  sudo chown root:nixbld '$CCACHE_DIR'"
-            echo "====="
-            exit 1
-          fi
-          if [ ! -w "$CCACHE_DIR" ]; then
-            echo "====="
-            echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
-            echo "Please verify its access permissions"
-            echo "====="
-            exit 1
-          fi
-        '';
-      };
-    })
   ];
 in {
   nixpkgs.overlays = packageOverlays;
