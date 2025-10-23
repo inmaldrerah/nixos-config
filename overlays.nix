45c49d6de03caad4ac2e4b2afab9ff313b920d67
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2025-10-23 03:54:27 +0000

    snapshot@10/23/2025-03:54:27

--- overlays.nix
+++ overlays.nix
@@ -1,7 +1,8 @@
-{ config, nixpkgsInput, nur-linyinfeng, ... }:
+{ config, nixpkgsInput, nixpkgs-extension, nur-linyinfeng, ... }:
 
 let
   packageOverlays = [
+    nixpkgs-extension.overlays.default
     nur-linyinfeng.overlays.default
   ];
 in {
