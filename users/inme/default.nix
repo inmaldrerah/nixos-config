7a194b9f930fa0eb2f22065135605d3d856dde7d
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2025-10-23 05:45:11 +0000

    snapshot@10/23/2025-05:45:11

--- users/inme/default.nix
+++ users/inme/default.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, ... }:
 {
-  shell = pkgs.shell;
+  shell = pkgs.xonsh;
   home-manager = { config, pkgs, nixvim, hm-extension, ... }: {
     imports = [
       nixvim.homeModules.nixvim
