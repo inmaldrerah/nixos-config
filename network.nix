480a7b5471787c81127e9be030ea56a6be48c5cb
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2025-10-27 17:47:29 +0000

    snapshot@10/27/2025-17:47:29

--- network.nix
+++ network.nix
@@ -44,7 +44,7 @@ rec {
 
   # Open ports in the firewall.
   networking.firewall = {
-    enable = false;
+    enable = true;
     allowedTCPPorts = [ 12345 ];
     allowedUDPPorts = [ 12345 ];
     trustedInterfaces = [ "lo" "tailscale0" ];
