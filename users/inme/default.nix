ea8b4c970564c61d50bdd8a2b5af502249cea47e
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2025-11-14 11:54:32 +0000

    snapshot@11/14/2025-11:54:32

--- users/inme/default.nix
+++ users/inme/default.nix
@@ -31,6 +31,18 @@
 
     programs.helix = {
       enable = true;
+      settings = {
+        editor.cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+      };
+      languages.language = [{
+        name = "nix";
+        auto-format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }]
     };
 
     programs.kitty = {
