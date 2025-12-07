d9b55bb5adb44c694c1167dce81d021534bfb6a5
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2025-12-07 16:11:52 +0000

    snapshot@12/07/2025-16:11:52

--- overlays.nix
+++ overlays.nix
@@ -12,46 +12,6 @@ let
     # nixpkgs-extension.overlays.default
     nix-vscode-extensions.overlays.default
     nur-linyinfeng.overlays.default
-    (self: super: {
-      python3Packages = super.python3Packages // {
-        xonsh = super.python3Packages.xonsh // {
-          xontribs.xontrib-sh =
-            with super;
-            with super.python3Packages;
-            (buildPythonPackage rec {
-              pname = "xontrib-sh";
-              version = "0.3.1";
-              pyproject = false;
-
-              src = fetchFromGitHub {
-                owner = "anki-code";
-                repo = "xontrib-sh";
-                tag = version;
-                hash = "sha256-R1DCGMrRCJLnz/QMk6QB8ai4nx88vvyPdaCKg3od5/I=";
-              };
-
-              build-system = [
-                setuptools
-              ];
-
-              nativeCheckInputs = [
-                writableTmpDirAsHomeHook
-                pytestCheckHook
-                xonsh
-              ];
-
-              passthru.updateScript = nix-update-script { };
-
-              meta = {
-                description = "Paste and run commands from bash, zsh, fish, tcsh, pwsh in xonsh shell";
-                homepage = "https://github.com/anki-code/xontrib-sh";
-                license = lib.licenses.mit;
-                maintainers = with lib.maintainers; [ ];
-              };
-            });
-        };
-      };
-    })
   ];
 in
 {
--- users/inme/xonsh.nix
+++ users/inme/xonsh.nix
@@ -80,7 +80,41 @@
     extraPackages = ps: [
       ps.xonsh.xontribs.xonsh-direnv
       ps.xonsh.xontribs.xontrib-fish-completer
-      ps.xonsh.xontribs.xontrib-sh
+      (
+        with pkgs;
+        with pkgs.python3Packages;
+        (buildPythonPackage rec {
+          pname = "xontrib-sh";
+          version = "0.3.1";
+          pyproject = false;
+
+          src = fetchFromGitHub {
+            owner = "anki-code";
+            repo = "xontrib-sh";
+            tag = version;
+            hash = "sha256-R1DCGMrRCJLnz/QMk6QB8ai4nx88vvyPdaCKg3od5/I=";
+          };
+
+          build-system = [
+            setuptools
+          ];
+
+          nativeCheckInputs = [
+            writableTmpDirAsHomeHook
+            pytestCheckHook
+            xonsh
+          ];
+
+          passthru.updateScript = nix-update-script { };
+
+          meta = {
+            description = "Paste and run commands from bash, zsh, fish, tcsh, pwsh in xonsh shell";
+            homepage = "https://github.com/anki-code/xontrib-sh";
+            license = lib.licenses.mit;
+            maintainers = with lib.maintainers; [ ];
+          };
+        })
+      )
     ];
   };
 }
