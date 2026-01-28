eedf580b5b85196ab54433f3a1e2fe3337cf01e7
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2026-01-28 06:10:07 +0000

    snapshot@01/28/2026-06:10:07

--- device/framework-nixos/default.nix
+++ device/framework-nixos/default.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   ...
 }:
@@ -13,19 +12,6 @@
     settings.GTK.theme_name = lib.mkForce "Adwaita-dark";
   };
 
-  # services.greetd = {
-  #   enable = true;
-  #   settings = rec {
-  #     initial_session = {
-  #       command = "${pkgs.uwsm}/bin/uwsm start ${pkgs.hyprland}/bin/Hyprland";
-  #       user = "inme";
-  #     };
-  #     default_session = initial_session;
-  #   };
-  # };
-
-  # Automatically log in to account
-  # Password already verified at startup
   programs.hyprland = {
     withUWSM = true;
   };
