4a118377dffd7ef8c3613cbeecb53c38f47725ef
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2026-06-13 15:41:35 +0000

    snapshot@06/13/2026-15:41:35

--- users/inme/default.nix
+++ users/inme/default.nix
@@ -122,7 +122,7 @@
         enable = true;
         settings = {
           general = {
-            lock_cmd = "noctalia-shell ipc call lockScreen lock";
+            lock_cmd = "noctalia msg session lock";
             before_sleep_cmd = "loginctl lock-session";
             after_sleep_cmd = "niri msg action power-on-monitors";
           };
