298289dc5c87804692bde081c9d6251271d3a731
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2026-08-13 14:11:54 +0000

    snapshot@08/13/2026-14:11:54

--- configuration.nix
+++ configuration.nix
@@ -26,17 +26,17 @@
       builders-use-substitutes = true;
       substituters = [
         "https://nix-community.cachix.org"
-        "https://cache.garnix.io"
+        # "https://cache.garnix.io"
         "https://cache.nixos.org"
       ];
       trusted-substituters = [
         "https://nix-community.cachix.org"
-        "https://cache.garnix.io"
+        # "https://cache.garnix.io"
         "https://cache.nixos.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
       trusted-users = [
         "root"
