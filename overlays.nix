897dad2865a4bd62a6aed770c4e898b9993130de
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2026-09-03 04:08:35 +0000

    snapshot@09/03/2026-04:08:35

--- overlays.nix
+++ overlays.nix
@@ -8,12 +8,10 @@ let
   packageOverlays = [
     inputs.nix4vscode.overlays.default
     inputs.nix-vscode-extensions.overlays.default
+    inputs.imap-to-jmap.overlays.stalwart-imap-proxy
     # inputs.nur-linyinfeng.overlays.default
   ];
-  imapToJmap = final: prev: {
-    stalwart-imap-proxy = inputs.imap-to-jmap.packages.${final.system}.default;
-  };
 in
 {
-  nixpkgs.overlays = packageOverlays ++ [ imapToJmap ];
+  nixpkgs.overlays = packageOverlays;
 }
