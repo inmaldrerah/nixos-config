f4103e36235cea03a4b8df46053c3f2c1bcdb531
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2026-09-20 08:36:20 +0000

    snapshot@09/20/2026-08:36:20

--- users/default.nix
+++ users/default.nix
@@ -46,7 +46,6 @@ in
       };
     };
 
-  # environment.persistence."/nix/persist".users.inme = userconf.inme.persistence;
   environment.persistence."/mnt/shared/linux-home/nixos".users.inme =
     userconf.inme.shared-persistence;
 }
